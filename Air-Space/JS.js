6f1a2791c52ef05dcde20f03ec688f6d1e67b467
masloamaxaslo9 <masloamaxaslo9@gmail.com>
2018-03-01 19:23:03 +0200

    Air-Space

---
+++ Air-Space/JS.js
@@ -0,0 +1,38 @@
+function name() {
+  var var1 = 1;
+  switch (var1){
+    case 1:
+      var var1 = "1d";
+      document.write(var1);
+      break;
+    case 2:
+      var var1 = "2d";
+      document.write(var1);
+      break;
+    case 3:
+      var var1 = "3d";
+      document.write(var1);
+      break;
+    case 4:
+      var var1 = "4d";
+      document.write(var1);
+      break;
+    case 5:
+      var var1 = "5d";
+      document.write(var1);
+      break;
+    case 6:
+      var var1 = "6d";
+      document.write(var1);
+      break;
+    case 7:
+      var var1 = "7d";
+      document.write(var1);
+      break;
+    default:
+      var var1 = "";
+      document.write("error 404" + var1);
+  }
+}
+
+name();
