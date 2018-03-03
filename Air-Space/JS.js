1b4a9b7846d673fe9f78bac8cb4891128ab62351
masloamaxaslo9 <masloamaxaslo9@gmail.com>
2018-03-03 19:30:38 +0200

    Air-Space: header over

--- Air-Space/JS.js
+++ Air-Space/JS.js
@@ -3,31 +3,24 @@ function name() {
   switch (var1){
     case 1:
       var var1 = "1d";
-      document.write(var1);
       break;
     case 2:
       var var1 = "2d";
-      document.write(var1);
       break;
     case 3:
       var var1 = "3d";
-      document.write(var1);
       break;
     case 4:
       var var1 = "4d";
-      document.write(var1);
       break;
     case 5:
       var var1 = "5d";
-      document.write(var1);
       break;
     case 6:
       var var1 = "6d";
-      document.write(var1);
       break;
     case 7:
       var var1 = "7d";
-      document.write(var1);
       break;
     default:
       var var1 = "";
@@ -36,3 +29,5 @@ function name() {
 }
 
 name();
+
+
